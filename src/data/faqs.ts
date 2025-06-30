a4f125949e425300dcb21b35bd45f6699797418e
Juan Jose Miranda <jjsmm97@gmail.com>
2025-06-29 20:30:10 -0400

    feat: add faqs

---
+++ src/data/faqs.ts
@@ -0,0 +1,62 @@
+export const faqs = [
+  {
+    question: "¿Qué es un AWS Community Day?",
+    answer:
+      "Los **AWS Community Day** son eventos organizado por la comunidad de usuarios de **Amazon Web Services (AWS),** donde se comparten conocimientos y experiencias sobre tecnologías en la nube, tendencias y buenas prácticas. Es una gran oportunidad para aprender y conectar con otros profesionales y entusiastas de la nube AWS.",
+  },
+  {
+    question: "¿Cuándo y dónde se llevará a cabo el AWS Community Day Bolivia 2025?",
+    answer:
+      "El **AWS Community Day Bolivia** se celebrará el **20 de septiembre de 2025** en la Universidad Católica Boliviana 'San Pablo' (UCB) en la ciudad de Cochabamba.",
+  },
+  {
+    question: "¿Hay algún costo para asistir al evento?",
+    answer:
+      "La asistencia al **AWS Community Day Bolivia 2025** es gratuita. Sin embargo, se requiere inscripción previa debido al cupo limitado.",
+  },
+  {
+    question: "¿Quién puede asistir al AWS Community Day Bolivia 2025?",
+    answer:
+      "El evento está abierto a todos, incluyendo desarrolladores, arquitectos de soluciones, ingenieros de sistemas, estudiantes y cualquier persona interesada en aprender más sobre AWS y la computación en la nube.",
+  },
+  {
+    question: "¿Cómo puedo registrarme para el evento?",
+    answer:
+      "Puedes registrarte para el evento a través de nuestra [web oficial](https://day.awscommunity.org.bo), haciendo clic en el botón **¡Regístrate!** Esto te llevará a la plataforma Eventbrite, en donde deberás llenar tus datos. El registro es obligatorio para asistir.",
+  },
+  {
+    question: "¿Qué puedo esperar del AWS Community Day Bolivia 2025?",
+    answer:
+      "Podrás asistir a charlas y talleres impartidos por expertos de la industria, hacer networking, y conocer las últimas tendencias, productos y tecnologías de AWS.",
+  },
+  {
+    question: "¿Se entregarán certificados de asistencia?",
+    answer:
+      "Todos los participantes recibirán un badge digital de asistencia los días posteriores al evento.",
+  },
+  {
+    question: "¿Se proporcionará algún tipo de refrigerio a los asistentes del evento?",
+    answer: "Sí, se proporcionarán de refrigerios para los asistentes a lo largo del evento.",
+  },
+  {
+    question:
+      "¿Cómo puedo involucrarme más en la comunidad de AWS después del evento?",
+    answer:
+      "Puedes unirte a grupos de usuarios locales de AWS, participar en eventos futuros, y seguirnos en nuestras redes sociales para mantenerte al día con las novedades y oportunidades.",
+  },
+  {
+    question: "¿Cómo puedo patrocinar el AWS Community Day Bolivia 2025?",
+    answer:
+      "Si estás interesado en patrocinar el evento, por favor envíanos un mensaje a: [sponsors@cloud.org.bo](mailto:sponsors@cloud.org.bo) ",
+  },
+  {
+    question: "¿Puedo proponer una charla o taller para el evento?",
+    answer:
+      "Sí, estamos abiertos a propuestas de charlas y talleres hasta el 27 de Julio de 2025. Puedes enviar tu propuesta a través de nuestro formulario de propuestas haciendo click [aquí](https://sessionize.com/aws-community-day-bolivia-2025/).",
+  },
+  {
+    question: "Si tengo más consultas, ¿cómo puedo comunicarme con los organizadores?",
+    answer:
+      "Puedes mandarnos un mensaje o puedes etiquetarnos en cualquiera de nuestras redes sociales: [Facebook](https://www.facebook.com/awscommunitydaybolivia), [Instagram](https://www.instagram.com/awscommunitydaybolivia), [LinkedIn](https://www.linkedin.com/company/aws-community-day-bolivia/) o escribirnos al correo: [contacto@cloud.org.bo](mailto:contacto@cloud.org.bo). Haremos todo lo posible para responder lo mas pronto posible. 🙂",
+  },
+];
